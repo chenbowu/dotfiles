c4b30c4943325dbc5baf6a84a8e75b76bfde7a39
chenboowu <1028505817@qq.com>
2023-02-18 21:42:35 +0800

    uninstall dashborad plugin of neovim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,8 +8,8 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use {
-      'svrana/neosolarized.nvim',
-      requires = { 'tjdevries/colorbuddy.nvim' }
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
   }
   use 'kyazdani42/nvim-web-devicons' -- File icons
   use 'glepnir/lspsaga.nvim'
@@ -21,8 +21,8 @@ packer.startup(function(use)
   use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- lsp
   use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
   } -- code highlight
   use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via lua
   use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in
@@ -34,30 +34,30 @@ packer.startup(function(use)
   use 'JoosepAlviste/nvim-ts-context-commentstring'
 
   use {
-      'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup {
-            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-        }
-      end,
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
   }
   use {
-      'kylechui/nvim-surround',
-      config = function()
-        require('nvim-surround').setup()
-      end,
+    'kylechui/nvim-surround',
+    config = function()
+      require('nvim-surround').setup()
+    end,
   }
   use {
-      'ggandor/lightspeed.nvim',
-      config = function()
-        require('lightspeed').setup {
-            exit_after_idle_msecs = {
-                labeled = 1500,
-                -- Increase timeout for f/t keys
-                unlabeled = 3000,
-            },
-        }
-      end,
+    'ggandor/lightspeed.nvim',
+    config = function()
+      require('lightspeed').setup {
+        exit_after_idle_msecs = {
+          labeled = 1500,
+          -- Increase timeout for f/t keys
+          unlabeled = 3000,
+        },
+      }
+    end,
   }
 
 
@@ -93,31 +93,31 @@ packer.startup(function(use)
   -- Git
   -- I only use the "blame" feature from this
   use {
-      'tpope/vim-fugitive',
-      cmd = { 'Git', 'G', 'Gstatus', 'Gblame', 'Gpush', 'Gpull', 'Gdiff' },
+    'tpope/vim-fugitive',
+    cmd = { 'Git', 'G', 'Gstatus', 'Gblame', 'Gpush', 'Gpull', 'Gdiff' },
   }
 
   use({
-      'glepnir/galaxyline.nvim',
-      brach = 'main',
-      config = function()
-        require('galaxyline');
-      end,
-      -- some optional icons
-      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    'glepnir/galaxyline.nvim',
+    brach = 'main',
+    config = function()
+      require('galaxyline');
+    end,
+    -- some optional icons
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   })
 
-  use {
-      'glepnir/dashboard-nvim',
-      event = 'VimEnter',
-      config = function()
-        require('dashboard').setup {
-            theme = 'hyper',
-            config = {
-            }
-            --config
-        }
-      end,
-      requires = { 'nvim-tree/nvim-web-devicons' }
-  }
+  -- use {
+  --     'glepnir/dashboard-nvim',
+  --     event = 'VimEnter',
+  --     config = function()
+  --       require('dashboard').setup {
+  --           theme = 'hyper',
+  --           config = {
+  --           }
+  --           --config
+  --       }
+  --     end,
+  --     requires = { 'nvim-tree/nvim-web-devicons' }
+  -- }
 end)
